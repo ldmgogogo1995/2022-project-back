3e2058120259bea09082d68c2574eb937a304620
luodaming <775495877@qq.com>
2021-12-01 01:03:42 +0800

    feat: 项目初始化

---
+++ app.ts
@@ -0,0 +1,13 @@
+/*
+ * @Author: ldm
+ * @Date: 2021-11-18 20:37:14
+ * @LastEditors: ldm
+ * @LastEditTime: 2021-11-20 01:56:05
+ * @Description: 后端服务入口文档
+ */
+const express = require('express')
+const app = express()
+
+app.listen('1995', () => {
+    console.log('server start in 1995..')
+})
---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+/*
+ * @Author: ldm
+ * @Date: 2021-11-18 20:37:37
+ * @LastEditors: ldm
+ * @LastEditTime: 2021-11-19 02:02:04
+ * @Description: webpack配置文件
+ */
+module.exports = (env, args) => {
+    console.log(env.devlopment)
+    const config  = {
+        mode:'development',
+        entry:''
+    }
+}
